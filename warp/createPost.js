d45c7f42760edabf898b45cf23f8e885161e2f77
jajablinky <gmillar6@gmail.com>
2022-12-09 23:24:38 -0800

    create post page works just need to work on home page

--- warp/createPost.js
+++ warp/createPost.js
@@ -4,7 +4,7 @@ import { v4 as uuid } from "uuid";
 
 async function createPost() {
   let wallet = await configureWallet();
-  const contract = warp.contract(transactionId.connect(wallet));
+  const contract = warp.contract(transactionId).connect(wallet);
 
   await contract.writeInteraction({
     function: "createPost",
